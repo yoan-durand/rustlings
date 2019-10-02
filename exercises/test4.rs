4f40a64a94b8a0d5b5fc5d0ab414de5bd6cf16c3
Yoan Durand <yoan@follow.fr>
2019-10-02 19:53:15 +0200

    exercise: macros test4

--- exercises/test4.rs
+++ exercises/test4.rs
@@ -5,8 +5,13 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
+macro_rules! my_macro {
+    ($e: expr) => {"Hello ".to_owned() + $e};
+}
+
 fn main() {
     if my_macro!("world!") != "Hello world!" {
         panic!("Oh no! Wrong output!");
     }
 }
+                                                                    
